06b2536a48bceb3f5a310d171b1804e4d9b4cef4
juparefe <jp_rendon93@hotmail.com>
2024-02-26 20:21:22 -0500

    Next commit

--- main.go
+++ main.go
@@ -2,36 +2,29 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 
-	exercises "github.com/juparefe/Golang-Ecommerce/exercises"
-	integers "github.com/juparefe/Golang-Ecommerce/variables"
+	exercises "github.com/juparefe/Golang-Ecommerce/learning/exercises"
+	files "github.com/juparefe/Golang-Ecommerce/learning/files"
+	iterations "github.com/juparefe/Golang-Ecommerce/learning/iterations"
+	keyboard "github.com/juparefe/Golang-Ecommerce/learning/keyboard"
+	variables "github.com/juparefe/Golang-Ecommerce/learning/variables"
 )
 
 func main() {
-	integers.ShowIntegers()
-	integers.OtherVariables()
-	status, text := integers.ConvertToText(1234)
-	fmt.Println("Correct:", status)
-	fmt.Println("Text:", text)
-	os := runtime.GOOS
-	if os == "Linux." {
-		fmt.Println("If simple: Si es Linux")
-	} else {
-		fmt.Println("If simple: No es Linux")
+	var showLearning = false
+	if showLearning {
+		variables.ShowIntegers()
+		variables.OtherVariables()
+		status, text := variables.ConvertToText(1234)
+		fmt.Println("Correct:", status)
+		fmt.Println("Text:", text)
+		variables.CheckSystem()
+		exercises.ConvertToInteger("500")
+		keyboard.NumbersInput()
+		iterations.Iterate()
+		fmt.Println(exercises.ValidateMistake())
+		files.SaveFile()
+		files.AppendFile()
 	}
-	if arq := runtime.GOARCH; arq == "amd64" && (os == "Linux." || os == "windows") {
-		fmt.Println("If compuesto: Si es amd64 y Linux-Windows")
-	} else {
-		fmt.Println("If compuesto: No es amd64 y Linux-Windows")
-	}
-	switch root := runtime.GOROOT(); root {
-	case "go":
-		fmt.Println("Switch: El root es la carpeta go")
-	default:
-		fmt.Println("Switch: El root es la carpeta", root)
-	}
-	number, hundred := exercises.ConvertToInteger("500")
-	fmt.Println("Exercise01:", number)
-	fmt.Println("Exercise01:", hundred)
+	files.ReadFile()
 }
