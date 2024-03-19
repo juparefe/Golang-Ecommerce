8ff5d8cde3729b99123b73719f40360abf681b87
juparefe <jp_rendon93@hotmail.com>
2024-03-19 13:41:27 -0500

    Next commit

--- awsgo/awsgo.go
+++ awsgo/awsgo.go
@@ -2,6 +2,7 @@ package awsgo
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -13,9 +14,11 @@ var err error
 
 func StartAWS() {
 	Context = context.TODO()
+	fmt.Println("Getting Context: ", Context)
 	Config, err = config.LoadDefaultConfig(Context, config.WithDefaultRegion("us-east-1"))
 
 	if err != nil {
+		fmt.Print("Error loading awsgo: ", err.Error())
 		panic("Error al cargar la configuracion .aws/config" + err.Error())
 	}
 }
