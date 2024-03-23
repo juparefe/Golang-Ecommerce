7a5fdd7af996789016673cf5b961e21137dfa2a0
juparefe <jp_rendon93@hotmail.com>
2024-03-22 22:18:19 -0500

    Next commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,16 +19,16 @@ func Handlers(path string, method string, body string, headers map[string]string
 	}
 
 	fmt.Println("Path to validate: ", path[0:5])
-	switch path[0:5] {
-	case "addre":
+	switch path[0:4] {
+	case "addr":
 		return ProcessAddresses(body, path, method, user, idn, request)
-	case "categ":
+	case "cate":
 		return ProcessCategories(body, path, method, user, idn, request)
-	case "order":
+	case "orde":
 		return ProcessOrders(body, path, method, user, idn, request)
-	case "produ":
+	case "prod":
 		return ProcessProducts(body, path, method, user, idn, request)
-	case "stock":
+	case "stoc":
 		return ProcessStock(body, path, method, user, idn, request)
 	case "user":
 		return ProcessUsers(body, path, method, user, id, request)
