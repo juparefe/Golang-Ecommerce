a556a862b50a462b041d81c0060f14722b952f86
juparefe <jp_rendon93@hotmail.com>
2024-08-08 22:45:13 -0500

    Next commit

--- db/currencies.go
+++ db/currencies.go
@@ -2,10 +2,9 @@ package db
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/juparefe/Golang-Ecommerce/models"
@@ -43,41 +42,47 @@ func SelectCurrency(BaseCurrency, TargetCurrency string) (models.Currency, error
 	return Currency, nil
 }
 
-func UpdateRatesFromAPI() error {
-	resp, err := http.Get(apiURL)
+func UpdateRatesFromAPI(BaseCurrency, TargetCurrency string) (models.Currency, error) {
+	apiKey := os.Getenv("UrlPrefix")
+	apiUrl := "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + BaseCurrency
+
+	resp, err := http.Get(apiUrl)
+	fmt.Println("Respuesta obtenida:", resp)
 	if err != nil {
-		return fmt.Errorf("error fetching rates: %v", err)
+		fmt.Println("Error fetching data from API: ", err.Error())
+		return models.Currency{}, err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("error reading response: %v", err)
-	}
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return fmt.Errorf("error reading response: %v", err)
+	// }
 
-	var rates ExchangeRates
-	if err := json.Unmarshal(body, &rates); err != nil {
-		return fmt.Errorf("error unmarshalling response: %v", err)
-	}
+	// var rates ExchangeRates
+	// if err := json.Unmarshal(body, &rates); err != nil {
+	// 	return fmt.Errorf("error unmarshalling response: %v", err)
+	// }
 
-	tx, err := Db.Begin()
-	if err != nil {
-		return fmt.Errorf("error starting transaction: %v", err)
-	}
+	// tx, err := Db.Begin()
+	// if err != nil {
+	// 	return fmt.Errorf("error starting transaction: %v", err)
+	// }
 
-	_, err = tx.Exec("DELETE FROM exchange_rates")
-	if err != nil {
-		tx.Rollback()
-		return fmt.Errorf("error clearing exchange rates: %v", err)
-	}
+	// _, err = tx.Exec("DELETE FROM exchange_rates")
+	// if err != nil {
+	// 	tx.Rollback()
+	// 	return fmt.Errorf("error clearing exchange rates: %v", err)
+	// }
 
-	for currency, rate := range rates.Rates {
-		_, err := tx.Exec("REPLACE INTO exchange_rates (currency, rate, last_updated) VALUES (?, ?, ?)", currency, rate, time.Now())
-		if err != nil {
-			tx.Rollback()
-			return fmt.Errorf("error updating database: %v", err)
-		}
-	}
+	// for currency, rate := range rates.Rates {
+	// 	_, err := tx.Exec("REPLACE INTO exchange_rates (currency, rate, last_updated) VALUES (?, ?, ?)", currency, rate, time.Now())
+	// 	if err != nil {
+	// 		tx.Rollback()
+	// 		return fmt.Errorf("error updating database: %v", err)
+	// 	}
+	// }
 
-	return tx.Commit()
+	// return tx.Commit()
+	return models.Currency{}, nil
 }
